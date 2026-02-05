eb83f9a2e1489ac674e76092652f86c0dd4d89d7
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-02-05 17:11:37 +0100

    Added function for html page

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from booking.models import Booking
 
 # Create your views here.
+
+def booking_list(request):
+    bookings = Booking.objects.all()
+    context = {
+        "booking_list": bookings,
+    }
+    return render(
+        request,
+        template_name = "booking/booking_list.html",
+        context=context,
+    )
